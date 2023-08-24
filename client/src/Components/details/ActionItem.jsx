2d164b26f4d2c9b90a713e57ebc50301c5ab91da
Hrishi <hrishikorhale2002@gmail.com>
2023-08-24 16:47:53 +0530

    Add ProductDetails And Responsiveness

--- client/src/Components/details/ActionItem.jsx
+++ client/src/Components/details/ActionItem.jsx
@@ -1,14 +1,32 @@
 import styled from "@emotion/styled";
 import { Box, Button } from "@mui/material";
 import { ShoppingCart as Cart, FlashOn as Flash } from "@mui/icons-material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const LeftContainer = styled(Box)`
-  min-width: 40%;
-  padding: 40px 0 0 80px;
-`;
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
+
+const LeftContainer = styled(Box)(({ theme }) => ({
+  minWidth: "40%",
+  padding: "40px 0 0 80px",
+  [theme.breakpoints.down("md")]: {
+    padding: "20px 40px",
+  },
+}));
 
 const Image = styled("img")({
   padding: "15px",
+  maxWidth: "100%", // Set maximum width to ensure responsiveness
+  height: "auto",
 });
 
 const StyledButton = styled(Button)`
@@ -20,21 +38,23 @@ const StyledButton = styled(Button)`
 
 const ActionItem = ({ product }) => {
   return (
-    <LeftContainer>
-      <Box style={{ border: "1px solid #f0f0f0", width: "90%" }}>
-        <Image src={product.detailUrl} />
-      </Box>
-      <StyledButton
-        style={{ marginRight: 10, background: "#ff9f00" }}
-        variant="contained"
-      >
-        <Cart />
-        Add to Cart
-      </StyledButton>
-      <StyledButton style={{ background: "#fb641b" }} variant="contained">
-        <Flash /> Buy Now
-      </StyledButton>
-    </LeftContainer>
+    <ThemeProvider theme={theme}>
+      <LeftContainer>
+        <Box style={{ border: "1px solid #f0f0f0", width: "90%" }}>
+          <Image src={product.detailUrl} />
+        </Box>
+        <StyledButton
+          style={{ marginRight: 10, background: "#ff9f00" }}
+          variant="contained"
+        >
+          <Cart />
+          Add to Cart
+        </StyledButton>
+        <StyledButton style={{ background: "#fb641b" }} variant="contained">
+          <Flash /> Buy Now
+        </StyledButton>
+      </LeftContainer>
+    </ThemeProvider>
   );
 };
 
--- client/src/Components/details/DetailView.jsx
+++ client/src/Components/details/DetailView.jsx
@@ -6,19 +6,38 @@ import { getProductDetails } from "../../redux/action/ProductAction";
 import ActionItem from "./ActionItem";
 import { Box, Grid, Typography } from "@mui/material";
 import styled from "@emotion/styled";
+import ProductDetail from "./ProductDetail";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 
 const Component = styled(Box)`
   margin-top: 55px;
   background: #f2f2f2;
 `;
 
-const Container = styled(Grid)`
-    background: #FFFFFF;
-    display: flex'
-`;
+const Container = styled(Grid)(({ theme }) => ({
+  background: "#FFFFFF",
+  display: "flex",
+  [theme.breakpoints.down("md")]: {
+    margin: 0,
+  },
+}));
 
 const RightContainer = styled(Grid)`
   margin-top: 50px;
+  padding: 0 20px;
+  max-width: 100%;
   & > p {
     margin-top: 10px;
   }
@@ -39,41 +58,44 @@ const DetailView = () => {
     if (product && id !== product.id) dispatch(getProductDetails(id));
   }, [dispatch, id, product, loading]);
   return (
-    <Component>
-      {product && Object.keys(product).length && (
-        <Container container>
-          <Grid item lg={4} md={4} sm={8} xs={12}>
-            <ActionItem product={product} />
-          </Grid>
-          <RightContainer item lg={8} md={8} sm={8} xs={12}>
-            <Typography>{product.title.longTitle}</Typography>
-            <Typography
-              style={{ marginTop: 5, color: "#878787", fontSize: 14 }}
-            >
-              8 Ratings & 1 Reviews
-              <span>
-                <img
-                  src={fassured}
-                  alt="fassured"
-                  style={{ width: 77, marginLeft: 20 }}
-                />
-              </span>
-            </Typography>
-            <Typography>
-              <span style={{ fontSize: 28 }}>₹{product.price.cost}</span>
-              &nbsp;&nbsp;&nbsp;
-              <span style={{ color: "#878787" }}>
-                <strike>₹{product.price.mrp}</strike>
-              </span>
-              &nbsp;&nbsp;&nbsp;
-              <span style={{ color: "#388E3C" }}>
-                {product.price.discount} off
-              </span>
-            </Typography>
-          </RightContainer>
-        </Container>
-      )}
-    </Component>
+    <ThemeProvider theme={theme}>
+      <Component>
+        {product && Object.keys(product).length && (
+          <Container container>
+            <Grid item lg={4} md={4} sm={8} xs={12}>
+              <ActionItem product={product} />
+            </Grid>
+            <RightContainer item lg={8} md={8} sm={12} xs={12}>
+              <Typography>{product.title.longTitle}</Typography>
+              <Typography
+                style={{ marginTop: 5, color: "#878787", fontSize: 14 }}
+              >
+                8 Ratings & 1 Reviews
+                <span>
+                  <img
+                    src={fassured}
+                    alt="fassured"
+                    style={{ width: 77, marginLeft: 20 }}
+                  />
+                </span>
+              </Typography>
+              <Typography>
+                <span style={{ fontSize: 28 }}>₹{product.price.cost}</span>
+                &nbsp;&nbsp;&nbsp;
+                <span style={{ color: "#878787" }}>
+                  <strike>₹{product.price.mrp}</strike>
+                </span>
+                &nbsp;&nbsp;&nbsp;
+                <span style={{ color: "#388E3C" }}>
+                  {product.price.discount} off
+                </span>
+              </Typography>
+              <ProductDetail product={product} />
+            </RightContainer>
+          </Container>
+        )}
+      </Component>
+    </ThemeProvider>
   );
 };
 
