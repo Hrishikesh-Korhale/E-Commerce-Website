85e2f3bbe00f8ed7eac6c0a1f467ac3569e57c13
Hrishi <hrishikorhale2002@gmail.com>
2023-08-24 18:59:42 +0530

    Enable Product Searching

--- client/src/Components/Header/Search.jsx
+++ client/src/Components/Header/Search.jsx
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
-import { Box, InputBase } from "@mui/material";
+import { Box, InputBase, List, ListItem } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../redux/action/ProductAction";
+import { Link } from "react-router-dom";
 
 const SearchContainer = styled(Box)`
   background: #fff;
@@ -22,13 +26,51 @@ const SearchIconWrapper = styled(Box)`
   display: flex;
 `;
 
+const ListWrapper = styled(List)`
+  position: absolute;
+  background: #ffffff;
+  color: #000;
+  margin-top: 36px;
+`;
+
 const Search = () => {
+  const [Text, setText] = useState("");
+
+  const { products } = useSelector((state) => state.getProducts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  const getText = (e) => setText(e);
   return (
     <SearchContainer>
-      <InputSearchBase placeholder="Search for products, brands and more" />
+      <InputSearchBase
+        placeholder="Search for products, brands and more"
+        onChange={(e) => getText(e.target.value)}
+        value={Text}
+      />
       <SearchIconWrapper>
         <SearchIcon />
       </SearchIconWrapper>
+      {Text && (
+        <ListWrapper>
+          {products
+            .filter((product) =>
+              product.title.longTitle.toLowerCase().includes(Text.toLowerCase())
+            )
+            .map((product) => (
+              <Link
+                to={`product/${product.id}`}
+                onClick={() => setText("")}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <ListItem>{product.title.longTitle}</ListItem>
+              </Link>
+            ))}
+        </ListWrapper>
+      )}
     </SearchContainer>
   );
 };
