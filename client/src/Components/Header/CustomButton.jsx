b5269c2ed42bf4ac55d865f6886e96d919bedf37
Hrishi <hrishikorhale2002@gmail.com>
2023-08-24 18:18:39 +0530

    Make Header Responsive

--- client/src/Components/Header/CustomButton.jsx
+++ client/src/Components/Header/CustomButton.jsx
@@ -5,32 +5,62 @@ import { useContext, useState } from "react";
 import LoginDialog from "../Login/LoginDialog";
 import { DataContex } from "../../contex/dataProvider";
 import Profile from "./Profile";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-const Wrapper = styled(Box)`
-  display: flex;
-  margin: 0 3% 0 auto;
-  & > button,
-  & > p,
-  & div {
-    margin-right: 40px;
-    font-size: 16px;
-    align-items: center;
-  }
-`;
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 
-const LoginButton = styled(Button)`
-  color: #2874f0;
-  background: #fff;
-  text-transform: none;
-  padding: 5px 40px;
-  border-radius: 2px;
-  box-shadow: none;
-  font-weight: 600;
-  height: 32px;
-`;
-const Container = styled(Box)`
-  display: flex;
-`;
+const Wrapper = styled(Box)(({ theme }) => ({
+  margin: "0 3% 0 auto",
+  display: "flex",
+  "& > *": {
+    marginRight: "40px !important",
+    textDecoration: "none",
+    color: "#FFFFFF",
+    fontSize: 12,
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      color: "#2874f0",
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "column",
+      marginTop: 10,
+    },
+  },
+  [theme.breakpoints.down("sm")]: {
+    display: "block",
+  },
+}));
+
+const LoginButton = styled(Button)(({ theme }) => ({
+  color: "#2874f0",
+  background: "#FFFFFF",
+  textTransform: "none",
+  fontWeight: 600,
+  borderRadius: 2,
+  padding: "5px 40px",
+  height: 32,
+  boxShadow: "none",
+  [theme.breakpoints.down("sm")]: {
+    background: "#2874f0",
+    color: "#FFFFFF",
+  },
+}));
+const Container = styled(Box)(({ theme }) => ({
+  display: "flex",
+  [theme.breakpoints.down("sm")]: {
+    display: "block",
+  },
+}));
 const CustomButton = () => {
   const [open, setOpen] = useState(false);
 
@@ -40,25 +70,27 @@ const CustomButton = () => {
     setOpen(true);
   };
   return (
-    <Wrapper>
-      {account ? (
-        <Profile account={account} setAccount={setAccount} />
-      ) : (
-        <LoginButton variant="contained" onClick={() => openDialog()}>
-          Login
-        </LoginButton>
-      )}
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        {account ? (
+          <Profile account={account} setAccount={setAccount} />
+        ) : (
+          <LoginButton variant="contained" onClick={() => openDialog()}>
+            Login
+          </LoginButton>
+        )}
 
-      <Typography style={{ marginTop: 3, width: 135 }}>
-        Become a Seller
-      </Typography>
-      <Typography style={{ marginTop: 3 }}>More</Typography>
-      <Container>
-        <ShoppingCartIcon />
-        <Typography>Cart</Typography>
-      </Container>
-      <LoginDialog open={open} setOpen={setOpen} />
-    </Wrapper>
+        <Typography style={{ marginTop: 3, width: 135 }}>
+          Become a Seller
+        </Typography>
+        <Typography style={{ marginTop: 3 }}>More</Typography>
+        <Container>
+          <ShoppingCartIcon />
+          <Typography>Cart</Typography>
+        </Container>
+        <LoginDialog open={open} setOpen={setOpen} />
+      </Wrapper>
+    </ThemeProvider>
   );
 };
 
--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,16 +1,43 @@
 import styled from "@emotion/styled";
-import { AppBar, Toolbar, Box, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+} from "@mui/material";
 import Search from "./Search";
 import CustomButton from "./CustomButton";
 import { Link } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Menu } from "@mui/icons-material";
+import { useState } from "react";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 
 const StyledHeader = styled(AppBar)`
   background: #2874f0;
   height: 55px;
 `;
-const CustomButtonWrapper = styled(Box)`
-  margin-left: auto;
-`;
+const CustomButtonWrapper = styled(Box)(({ theme }) => ({
+  marginLeft: "auto",
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
+}));
 
 const Component = styled(Link)`
   margin-left: 12%;
@@ -28,32 +55,66 @@ const PlusImg = styled("img")({
   marginLeft: 4,
 });
 
+const MenuButton = styled(IconButton)(({ theme }) => ({
+  display: "none",
+  [theme.breakpoints.down("md")]: {
+    display: "block",
+  },
+}));
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   const logoURL =
     "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
   const subURL =
     "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
-  return (
-    <StyledHeader>
-      <Toolbar style={{ minHeight: 55 }}>
-        <Component to={`/`}>
-          <img src={logoURL} alt="Logo" style={{ width: 75 }} />
-          <Box display={"flex"}>
-            <SubHeading>
-              Explore &nbsp;
-              <Box component="span" style={{ color: "#ffe500" }}>
-                Plus
-              </Box>
-            </SubHeading>
-            <PlusImg src={subURL} alt="sublogo" />
-          </Box>
-        </Component>
-        <Search />
-        <CustomButtonWrapper>
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const list = () => (
+    <Box style={{ width: 200 }} onClick={handleClose}>
+      <List>
+        <ListItem button>
           <CustomButton />
-        </CustomButtonWrapper>
-      </Toolbar>
-    </StyledHeader>
+        </ListItem>
+      </List>
+    </Box>
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <StyledHeader>
+        <Toolbar style={{ minHeight: 55 }}>
+          <MenuButton onClick={handleOpen} color="inherit">
+            <Menu />
+          </MenuButton>
+          <Drawer open={open} onClose={handleClose}>
+            {list()}
+          </Drawer>
+          <Component to={`/`}>
+            <img src={logoURL} alt="Logo" style={{ width: 75 }} />
+            <Box display={"flex"}>
+              <SubHeading>
+                Explore &nbsp;
+                <Box component="span" style={{ color: "#ffe500" }}>
+                  Plus
+                </Box>
+              </SubHeading>
+              <PlusImg src={subURL} alt="sublogo" />
+            </Box>
+          </Component>
+          <Search />
+          <CustomButtonWrapper>
+            <CustomButton />
+          </CustomButtonWrapper>
+        </Toolbar>
+      </StyledHeader>
+    </ThemeProvider>
   );
 };
 
